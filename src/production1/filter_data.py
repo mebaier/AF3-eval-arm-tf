e1e29cc0d80e892a1e6a5e932bfdf7dedc69c1a8
Markus Baier <m.elias.baier@posteo.net>
2025-07-17 10:24:14 +0200

    put filter lists in extra file

---
+++ src/production1/filter_data.py
@@ -0,0 +1,50 @@
+"""
+Filter data for protein domain identification.
+
+This module contains accession lists for identifying proteins with
+armadillo repeat domains from InterPro and Pfam databases.
+"""
+
+# All InterPro IDs that have the word "armadillo" in the description
+arm_accs_ipr = [
+    'IPR013636',
+    'IPR024574',
+    'IPR041322',
+    'IPR055241',
+    'IPR056252',
+    'IPR016617',
+    'IPR031524',
+    'IPR038739',
+    'IPR038905',
+    'IPR039868',
+    'IPR040268',
+    'IPR042462',
+    'IPR042834',
+    'IPR043379',
+    'IPR044282',
+    'IPR051303',
+    'IPR052441',
+    'IPR011989',
+    'IPR016024',
+    'IPR000225',
+    'IPR041209',
+    'IPR049152',
+    'IPR006911'
+]
+
+# All Pfam accessions that have "armadillo" in the description (with version numbers)
+arm_accs_pfam = [
+    'PF00514.29',
+    'PF17822.6',
+    'PF08427.15',
+    'PF15767.10',
+    'PF22915.1',
+    'PF04826.19',
+    'PF23295.1',
+    'PF16629.10',
+    'PF18770.7',
+    'PF21052.3',
+    'PF11841.14',
+    'PF14726.11',
+    'PF18581.7'
+]
