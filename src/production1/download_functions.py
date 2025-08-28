be375ef41cadd3c27b9df302f12a1fd63fa44a90
Markus Baier <m.elias.baier@posteo.net>
2025-08-28 17:38:05 +0200

    bugfix

--- src/production1/download_functions.py
+++ src/production1/download_functions.py
@@ -107,7 +107,8 @@ def download_pdb_structures(pdb_ids: set, output_dir="/home/markus/MPI_local/dat
 
     for pdb_id in pdb_ids:
         if downloaded_count % 10 == 0:
-            print(f"downloaded {downloaded_count} of {len(pdb_ids)}.")
+            if debug:
+                print(f"downloaded {downloaded_count} of {len(pdb_ids)}.")
         if pd.isna(pdb_id):  # Skip NaN values
             continue
 
